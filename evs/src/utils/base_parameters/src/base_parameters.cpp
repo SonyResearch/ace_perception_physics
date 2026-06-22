2e6f3e479255bdb84d4e3e0ff6adc6551e1e2e82
Asude <asudeeaydin@gmail.com>
2026-06-22 13:00:09 +0200

    Bug fix

--- evs/src/utils/base_parameters/src/base_parameters.cpp
+++ evs/src/utils/base_parameters/src/base_parameters.cpp
@@ -84,7 +84,7 @@ bool BaseParameters::ReadParametersFromFile() {
 
 bool BaseParameters::ReadParametersFromString(const std::string &yaml_str) {
   try {
-    yaml_node_ = YAML::LoadString(yaml_str);
+    yaml_node_ = YAML::Load(yaml_str);
   } catch (YAML::Exception &e) {
     LOG(ERROR) << e.what() << "\n";
     return false;
